c6ee2af78c1e975e76a34615168d4aef886cbb7b
MaximoDydyuk <maximodydyuk@ipvc.pt>
2025-07-01 23:34:26 +0100

    Update passport-config.js

--- backend/config/passport-config.js
+++ backend/config/passport-config.js
@@ -3,6 +3,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
+// Estratégia local de autenticação
 passport.use(new LocalStrategy(
   async (username, password, done) => {
     try {
@@ -12,26 +13,33 @@ passport.use(new LocalStrategy(
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
-      if (isMatch) {
-        return done(null, user);
-      } else {
+      if (!isMatch) {
         return done(null, false, { message: 'Senha incorreta.' });
       }
+
+      return done(null, user);
     } catch (err) {
       return done(err);
     }
   }
 ));
 
+// Serialização: guarda apenas o ID do utilizador na sessão
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+// Desserialização: obtém o utilizador pelo ID guardado na sessão
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
+    if (!user) {
+      return done(new Error('Utilizador não encontrado durante a desserialização'));
+    }
     done(null, user);
   } catch (err) {
     done(err);
   }
 });
+
+module.exports = passport;
