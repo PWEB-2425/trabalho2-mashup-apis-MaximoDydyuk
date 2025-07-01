1c35d96507913f3a8e695a4c2d67ef8898a5ce9b
MaximoDydyuk <maximodydyuk@ipvc.pt>
2025-07-01 13:42:52 +0100

    adição do render app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://trabalho2-mashup-apis-maximodydyuk-r1fm.onrender.com/api';
 const appContainer = document.getElementById('app');
 // Estado da aplicação
 let currentUser = null;
