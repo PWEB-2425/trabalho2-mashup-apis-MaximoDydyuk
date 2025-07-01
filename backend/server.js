ab4cd6a6c83110cb8dde3f8367121faeb67b3658
MaximoDydyuk <maximodydyuk@ipvc.pt>
2025-07-01 13:51:53 +0100

    Update aos cookies server.js

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,8 @@ app.use(session({
   cookie: {
     maxAge: 24 * 60 * 60 * 1000, // 1 dia
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+    secure: true,
+    sameSite: 'none'
   }
 }));
 
