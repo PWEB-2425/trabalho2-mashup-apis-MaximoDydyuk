69c854027e776a4cdf9091f913e97774bf8570b1
MaximoDydyuk <maximodydyuk@ipvc.pt>
2025-07-01 13:57:02 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
-// Adicione antes das rotas:
+
 app.options('*', cors());
 
 // Middlewares - Aumentar o limite para JSON
