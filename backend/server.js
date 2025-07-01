654b896095e0670b2e19c52ca99b29080f5962f0
MaximoDydyuk <maximodydyuk@ipvc.pt>
2025-07-01 21:36:48 +0100

    Upd server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,8 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
-
+}));
+    
 // Middlewares - Aumentar o limite para JSON
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
